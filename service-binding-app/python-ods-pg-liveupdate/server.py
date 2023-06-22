57a21a07568266b7e377d972d3edaa17bc2529af
Jeffrey Wang <jeffrye@vmware.com>
2023-06-22 21:52:52 +0800

    add dev custom resource

---
+++ service-binding-app/python-ods-pg-liveupdate/server.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template
+from flask_healthz import healthz
+	
+app = Flask(__name__)
+HEALTHZ = {
+    "livez": "server.checks.liveness",
+    "readyz": "server.checks.readiness",
+}
+		
+app.config['HEALTHZ'] = HEALTHZ
+
+#app.register_blueprint(healthz, url_prefix="/healthz")
+
+#def liveness():
+#    pass
+
+#def readiness():
+#    pass
+
+@app.route('/livez')
+def liveness():
+    return {}
+
+@app.route('/readyz')
+def readiness():
+    return {}
+
+@app.route('/')
+def hello_world():
+    return "hello world!"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
